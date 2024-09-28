598970f07f6990fb834705119ffc555d280a4045
Ștefan Alecu <165364995+overanalytcl@users.noreply.github.com>
2024-09-29 02:27:11 +0300

    feat: minify CSS + improve package.json

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,52 @@
-const webpack = require('webpack');
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+
+const isProduction = process.env.NODE_ENV === 'production';
 
 const config = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader'
-        ]
-      },
-      {
-        test: /\.png$/,
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              mimetype: 'image/png'
-            }
-          }
-        ]
-      },
-      {
-        test: /\.svg$/,
-        use: 'file-loader'
-      }
-    ]
-  }
+   entry: './src/scss/main.scss',
+   output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'theme.js',
+   },
+   module: {
+      rules: [
+         {
+            test: /\.css$/,
+            use: [MiniCssExtractPlugin.loader, 'css-loader'],
+         },
+         {
+            test: /\.scss$/,
+            use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+         },
+         {
+            test: /\.png$/,
+            use: [
+               {
+                  loader: 'url-loader',
+                  options: {
+                     mimetype: 'image/png',
+                  },
+               },
+            ],
+         },
+         {
+            test: /\.svg$/,
+            use: 'file-loader',
+         },
+      ],
+   },
+   plugins: [
+      new MiniCssExtractPlugin({
+         filename: 'theme.css',
+      }),
+   ],
+   optimization: {
+      minimize: isProduction,
+      minimizer: [`...`, ...(isProduction ? [new CssMinimizerPlugin()] : [])],
+   },
+   mode: isProduction ? 'production' : 'development',
 };
 
 module.exports = config;
